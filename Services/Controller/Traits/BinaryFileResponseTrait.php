f2b6c715c8a17016e999668c4bdf3be818cd34b4
ProklUng <funtik21975@gmail.com>
2021-07-01 16:26:29 +0300

    BinaryFileResponseTrait

---
+++ Services/Controller/Traits/BinaryFileResponseTrait.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Prokl\FrameworkExtensionBundle\Services\Controller\Traits;
+
+use InvalidArgumentException;
+use LogicException;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use Symfony\Component\Mime\FileinfoMimeTypeGuesser;
+
+/**
+ * Trait BinaryFileResponseTrait
+ * @package Prokl\FrameworkExtensionBundle\Services\Controller\Traits
+ *
+ * @since 01.07.2021
+ */
+trait BinaryFileResponseTrait
+{
+    /**
+     * returnFile
+     *
+     * @param string $file Файл.
+     *
+     * @return BinaryFileResponse
+     *
+     * @throws InvalidArgumentException
+     * @throws LogicException
+     */
+    protected function returnFile(string $file) : BinaryFileResponse
+    {
+        $contentType = 'text/plain';
+
+        if (class_exists(FileinfoMimeTypeGuesser::class)) {
+            $mimeTypeGuesser = new FileinfoMimeTypeGuesser();
+            if ($mimeTypeGuesser->isGuesserSupported()) {
+                $contentType = $mimeTypeGuesser->guessMimeType($file) ?? $contentType;
+            }
+        }
+
+        return new BinaryFileResponse(
+            $file,
+            Response::HTTP_CREATED,
+            [
+                'Content-Type' => $contentType,
+            ],
+            false,
+            ResponseHeaderBag::DISPOSITION_ATTACHMENT
+        );
+    }
+}
